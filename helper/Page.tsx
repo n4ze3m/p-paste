27bc9b7904162b998acd4ccbfa073c0390d8dccc
n4ze3m <n4ze3m@gmail.com>
2022-12-07 09:34:43 +0530

    UI issue fixed

--- helper/Page.tsx
+++ helper/Page.tsx
@@ -3,14 +3,18 @@ import Header from "./Header.tsx";
 
 export function Page({ children }: { children: ComponentChildren }) {
   return (
-    <>
+    <div className="flex flex-col h-screen justify-between">
+      <div className="h-10">
       <Header />
-      <div class="p-4 mx-auto max-w-screen-md">
-        {children}
       </div>
-      <a href="https://fresh.deno.dev"
-      className="fixed bottom-0 right-0 p-4"
-      >
+      <main class="p-4 mx-auto max-w-screen-md w-full">
+        {children}
+      </main>
+      <footer className="h-10 my-3">
+        <a
+          href="https://fresh.deno.dev"
+          className="bottom-0 right-0 p-4 fixed"
+        >
           <img
             width="197"
             height="37"
@@ -18,6 +22,7 @@ export function Page({ children }: { children: ComponentChildren }) {
             alt="Made with Fresh"
           />
         </a>
-    </>
+      </footer>
+    </div>
   );
 }
